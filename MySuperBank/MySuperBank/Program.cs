08c2b69c08b65904f85ec332de8cfb732a82c2eb
Melinda Ivanov <jns.melinda@gmail.com>
2022-02-17 14:39:43 -0800

    initial superbank

---
+++ MySuperBank/MySuperBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

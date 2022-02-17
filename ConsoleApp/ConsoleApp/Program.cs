49ccc8d06b564f89c6f5a0ce2a892f649b45efb0
Melinda Ivanov <jns.melinda@gmail.com>
2022-02-17 12:57:09 -0800

    initial project

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

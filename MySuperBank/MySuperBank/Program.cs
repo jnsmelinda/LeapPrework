b8613e851dc1655a7bbda6f9e3b00d8c230dc0fc
Melinda Ivanov <jns.melinda@gmail.com>
2022-02-17 16:01:13 -0800

    creating bank

--- MySuperBank/MySuperBank/Program.cs
+++ MySuperBank/MySuperBank/Program.cs
@@ -6,7 +6,46 @@ namespace MySuperBank
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new BankAccount("Melcsi", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with ${account.Balance} initial balance.");
+
+            var account2 = new BankAccount("Joni", 1000000);
+            Console.WriteLine($"Account {account2.Number} was created for {account2.Owner} with ${account2.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Hammpock");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            // Test that the initial balances must be positive.
+            BankAccount invalidAccount;
+                try
+                {
+                    invalidAccount = new BankAccount("invalid", -55);
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine("Exception caught creating account with negative balance");
+                    Console.WriteLine(e.ToString());
+                }
+
+            account.MakeWithdrawal(50, DateTime.Now, "XBox");
+            Console.WriteLine(account.Balance);
+
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
         }
     }
 }
